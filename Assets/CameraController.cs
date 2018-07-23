8bd9f0f4522063005220fcf52f34bc86611cdaa4
voxace <dave@voxace.com>
2018-07-23 21:33:12 +1000

    Camera follow

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // Calculate the offset between the player and camera
+        offset = transform.position - player.transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        // Move camera to player position + update
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Update called before physics calculations
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+        Vector3 force = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(force);
+    }
+}
